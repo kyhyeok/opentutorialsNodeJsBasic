b3f8d8f00a4ee0f272a9af27aab75d650fb54951
kyhyeok <kyhyeok@gmail.com>
2020-10-14 17:55:23 +0900

    함수를 이용 정리 정돈하기

--- main.js
+++ main.js
@@ -7,63 +7,55 @@ const app = http.createServer(function (request, response) {
   const pathname = url.parse(requestUrl, true).pathname;
   let i = 0;
 
+  const templateHTML = (title, list, body) => `
+  <!doctype html>
+  <html>
+    <head>
+      <title>WEB2 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+    </body>
+  </html>
+  `;
+
+  const templateList = (fileList) => {
+    let list = "<ul>";
+    while (i < fileList.length) {
+      list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+      i++;
+    }
+    list = list + "<ul>";
+    return list;
+  };
+
   if (pathname === "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", (err, fileList) => {
         const title = "Welcome";
         const description = "Welcome node.js";
-        let list = "<ul>";
-        while (i < fileList.length) {
-          list =
-            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-          i++;
-        }
-        list = list + "<ul>";
-        const template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB2 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-              ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-        </html>
-        `;
+        const list = templateList(fileList);
+        const template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         response.writeHead(200);
         response.end(template);
       });
     } else {
       const title = queryData.id;
-      fs.readFile(`data/${title}`, "utf-8", (err, description) => {
-        fs.readdir("./data", (err, fileList) => {
-          let list = "<ul>";
-          while (i < fileList.length) {
-            list =
-              list +
-              `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-            i++;
-          }
-          list = list + "<ul>";
-          const template = `
-          <!doctype html>
-          <html>
-            <head>
-              <title>WEB2 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-                ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-          </html>
-          `;
+      fs.readdir("./data", (err, fileList) => {
+        fs.readFile(`data/${title}`, "utf-8", (err, description) => {
+          const list = templateList(fileList);
+          const template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
           response.writeHead(200);
           response.end(template);
         });
