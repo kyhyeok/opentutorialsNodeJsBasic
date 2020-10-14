e2bfb36c6f8d1bdaf00fe46a532e8986173d1347
kyhyeok <kyhyeok@gmail.com>
2020-10-14 16:59:33 +0900

    홈페이지 구현

--- main.js
+++ main.js
@@ -5,33 +5,58 @@ const app = http.createServer(function (request, response) {
   const requestUrl = request.url;
   const queryData = url.parse(requestUrl, true).query;
   const pathname = url.parse(requestUrl, true).pathname;
-  let title = queryData.id;
-  let template;
 
   if (pathname === "/") {
-    fs.readFile(`data/${title}`, "utf-8", (err, description) => {
-      template = `
-      <!doctype html>
-      <html>
-      <head>
-      <title>WEB2 - ${title}</title>
-      <meta charset="utf-8">
-      </head>
-      <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-      </body>
-      </html>
-      `;
+    if (queryData.id === undefined) {
+      const title = "Welcome";
+      const description = "Welcome node.js";
+      const template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB2 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+          </html>
+          `;
       response.writeHead(200);
       response.end(template);
-    });
+    } else {
+      const title = queryData.id;
+      fs.readFile(`data/${title}`, "utf-8", (err, description) => {
+        const template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB2 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
