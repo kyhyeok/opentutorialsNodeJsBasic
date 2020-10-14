6c203797cf2358b08b47778afed6a2dd10b954d9
kyhyeok <kyhyeok@gmail.com>
2020-10-14 16:42:42 +0900

    Not found 오류 구현

--- main.js
+++ main.js
@@ -4,36 +4,37 @@ const url = require("url");
 const app = http.createServer(function (request, response) {
   const requestUrl = request.url;
   const queryData = url.parse(requestUrl, true).query;
+  const pathname = url.parse(requestUrl, true).pathname;
   let title = queryData.id;
   let template;
-  if (requestUrl == "/") {
-    title = "Welcome";
-  }
-  if (requestUrl == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, "utf-8", (err, description) => {
-    template = `
-    <!doctype html>
-    <html>
+
+  if (pathname === "/") {
+    fs.readFile(`data/${title}`, "utf-8", (err, description) => {
+      template = `
+      <!doctype html>
+      <html>
       <head>
-        <title>WEB2 - ${title}</title>
-        <meta charset="utf-8">
+      <title>WEB2 - ${title}</title>
+      <meta charset="utf-8">
       </head>
       <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>${description}</p>
       </body>
-    </html>
-    `;
-    response.end(template);
-  });
+      </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(3000);
