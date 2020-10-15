615e6acb9a041db4043aa7d027ca68a8175fb1ba
kyhyeok <kyhyeok@gmail.com>
2020-10-15 12:07:04 +0900

    모듈의 활용

--- main.js
+++ main.js
@@ -2,34 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const querystring = require("querystring");
-
-const template = {
-  html: (title, list, body, control) => `
-  <!doctype html>
-  <html>
-    <head>
-      <title>WEB2 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB2</a></h1>
-        ${list}
-        ${control}
-        ${body}
-    </body>
-  </html>
-  `,
-  list: (fileList) => {
-    let i = 0;
-    let list = "<ul>";
-    while (i < fileList.length) {
-      list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-      i++;
-    }
-    list = list + "</ul>";
-    return list;
-  }
-};
+const template = require("./lib/template");
 
 const app = http.createServer((request, response) => {
   const requestUrl = request.url;
