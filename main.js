51af9db545ffdb7bac63db072e122118d42d559a
kyhyeok <kyhyeok@gmail.com>
2020-10-15 09:45:45 +0900

    글생성 UI 만들기

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-const app = http.createServer(function (request, response) {
-  const requestUrl = request.url;
-  const queryData = url.parse(requestUrl, true).query;
-  const pathname = url.parse(requestUrl, true).pathname;
-  let i = 0;
 
-  const templateHTML = (title, list, body) => `
+const templateHTML = (title, list, body) => `
   <!doctype html>
   <html>
     <head>
@@ -15,23 +10,29 @@ const app = http.createServer(function (request, response) {
       <meta charset="utf-8">
     </head>
     <body>
-      <h1><a href="/">WEB</a></h1>
+      <h1><a href="/">WEB2</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
     </body>
   </html>
   `;
 
-  const templateList = (fileList) => {
-    let list = "<ul>";
-    while (i < fileList.length) {
-      list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-      i++;
-    }
-    list = list + "<ul>";
-    return list;
-  };
+const templateList = (fileList) => {
+  let i = 0;
+  let list = "<ul>";
+  while (i < fileList.length) {
+    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i++;
+  }
+  list = list + "</ul>";
+  return list;
+};
 
+const app = http.createServer(function (request, response) {
+  const requestUrl = request.url;
+  const queryData = url.parse(requestUrl, true).query;
+  const pathname = url.parse(requestUrl, true).pathname;
   if (pathname === "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", (err, fileList) => {
@@ -61,6 +62,24 @@ const app = http.createServer(function (request, response) {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", (err, fileList) => {
+      const title = "WEB - create";
+      const list = templateList(fileList);
+      const template = templateHTML(
+        title,
+        list,
+        `
+          <form action="localhost:3000/precess_create" method="post">
+            <p><input type="text" name="title" placeholder="title" /></p>
+            <p><textarea name="description" placeholder="description"></textarea></p>
+            <p><input type="submit"></p>
+          </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
