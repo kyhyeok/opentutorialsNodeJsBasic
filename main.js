3c8b86685962a2f89e46e28f43d36efac44b88c7
kyhyeok <kyhyeok@gmail.com>
2020-10-16 13:53:36 +0900

    저자목록 보기 기능 구현

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const querystring = require("querystring");
 const template = require("./lib/template");
 const db = require("./lib/db");
 const topic = require("./lib/topic");
+const author = require("./lib/author");
 
 const app = http.createServer((request, response) => {
   const requestUrl = request.url;
@@ -25,6 +26,8 @@ const app = http.createServer((request, response) => {
     topic.update_process(request, response);
   } else if (pathname === "/delete_process") {
     topic.delete_process(request, response);
+  } else if (pathname === "/author") {
+    author.home(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
